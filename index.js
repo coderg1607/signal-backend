33f1927ac71689ead3ae5b8a28f51a93673999a9
coderg1607 <71881776+coderg1607@users.noreply.github.com>
2021-05-28 14:43:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketio=require('socket.io');
 const http=require('http');
 const {addUser,removeUser,getUser,getUserInRoom} = require('./users.js');
-const cor=require(cors);
+const cor=require('cors');
 
 const PORT = process.env.PORT || 5000;
 
