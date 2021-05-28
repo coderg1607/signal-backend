6e256a5818f93ad8f8ece2211642a3c226bb5f02
gautam  ajugiya <gautamajugiya11111@gmail.com>
2021-05-28 12:57:17 +0530

    "first commit"

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const socketio=require('socket.io');
+const http=require('http');
+const {addUser,removeUser,getUser,getUserInRoom} = require('./users.js');
+const cor=require(cors);
+
+const PORT = process.env.PORT || 5000;
+
+const app=express();
+
+const router= require('./router');
+app.use(router);
+app.use(cor())
+const server=http.createServer(app);
+
+const io= socketio(server);
+io.on('connection',(socket)=>{
+    socket.on('join',({name,room},callback)=>{
+        const {error ,user } =addUser({id:socket.id,name,room});
+
+        if(error)
+        return callback(error);
+
+        socket.emit('message',{user:'admin', text : `${user.name},Welcome to the my whatsapp ${user.room} group`})
+        socket.broadcast.to(user.room).emit('message',{user:'admin',text:`${user.name} has joined group chat!`})
+        socket.join(user.room);
+        io.to(user.room).emit('roomData',{room:user.room , users:getUserInRoom(user.room)})
+        callback();
+    })
+    socket.on('sendMessage',(message,callback)=>{
+        const user=getUser(socket.id);
+        io.to(user.room).emit('message',{user:user.name , text : message});
+        io.to(user.room).emit('roomData',{room:user.room , users:getUserInRoom(user.room)});
+        callback();
+    })
+    socket.on('disconnect',()=>{
+        const user=removeUser(socket.id);
+
+        if(user)
+        {
+            io.to(user.room).emit('message',{user:'admin',text:`${user.name} has left the room!`})
+        }
+    })
+})
+server.listen(PORT,()=> console.log(`server started on ${PORT}`))
---
+++ users.js
@@ -0,0 +1,27 @@
+const users=[];
+const addUser=({id,name,room})=>{
+    name=name.trim().toLowerCase();
+    room=room.trim().toLowerCase()
+    const existUser=users.find((user)=> user.name===name && user.room===room);
+    if(existUser)
+    {
+        return {error:'username is already taken'};
+        
+    }
+    const user={id,name,room};
+    users.push(user);
+    return {user};
+}
+const removeUser=(id)=>{
+    const index=users.findIndex((user)=>user.id===id);
+    if(index!==-1)
+    {
+        return users.splice(index,1)[0];
+    }
+
+}
+const getUser=(id)=>users.find((user)=>users.id===id);
+
+const getUserInRoom=(room)=>users.filter((user)=>user.room===room);
+
+module.exports={addUser,removeUser,getUser,getUserInRoom};
